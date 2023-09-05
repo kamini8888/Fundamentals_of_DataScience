26f53e32cc8992410a8e398282da951d35ce8861
kamini8888 <112712900+kamini8888@users.noreply.github.com>
2023-09-05 08:20:47 +0530

    Update 39.py

--- 39.py
+++ 39.py
@@ -1,30 +1,30 @@
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+data={
+    'customer_id':[1,2,3,4,5],
+    'spending':[1000,2000,3000,4000,5000],
+    'purchase_frequency':[5,6,7,11,2]}
+data=pd.DataFrame(data)
+print(data)
+selected_features = ['customer_id', 'spending', 'purchase_frequency']
+X = data[selected_features]
 
-df = pd.read_csv("C:/Users/mailm/Downloads/customer_data2.csv")
+num_clusters = 3
+n_init=10
+random_state = 42
+kmeans = KMeans(n_clusters=num_clusters, n_init=n_init,random_state=random_state)
+clusters = kmeans.fit_predict(X) + 1
 
-
-df_encoded = pd.get_dummies(df, columns=['PurchaseBehavior'], drop_first=True)
-
-
-X = df_encoded[['SpendingScore']].values
-
-
-kmeans = KMeans(n_clusters=3, random_state=42, n_init=10)
-kmeans.fit(X)
-
-
-df['Cluster'] = kmeans.labels_
-
-
-plt.figure(figsize=(8, 6))
-plt.scatter(df['SpendingScore'], np.zeros(len(df)), c=df['Cluster'], cmap='viridis', s=100)
-plt.xlabel('Spending Score')
-plt.title('Customer Clusters')
+data['cluster'] = clusters
+cluster_means = data.groupby('cluster')[selected_features].mean()
+print(cluster_means)
+plt.scatter(data['customer_id'], data['spending'], c=data['cluster'], cmap='rainbow')
+plt.xlabel('Age')
+plt.ylabel('Income')
+plt.title('Customer Segmentation')
+plt.colorbar(label='Cluster')
 plt.show()
 
 
-print("Cluster Centers (Mean Spending Score):")
-print(kmeans.cluster_centers_)
